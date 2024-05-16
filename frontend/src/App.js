7c192a558bd8417dabf93f792e1dc68a5f93385e
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-16 15:58:04 +0100

    UI: folder structure set up, theme definitions

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from 'styled-components';
 
-function App() {
+// STYLES
+import { GlobalStyles } from './styles/GlobalStyles';
+import { theme } from './styles/Theme';
+
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <h1>Hello world!</h1>
+      <p>FinWise App</p>
+      <a>Test link</a>
+    </ThemeProvider>
   );
-}
-
-export default App;
+};
