8ba68e6b53d3b0d048239fbc6b7955cd5f61d29d
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-16 17:08:43 +0100

    UI: updated link css

--- frontend/src/components/Link/Link.style.js
+++ frontend/src/components/Link/Link.style.js
@@ -4,8 +4,7 @@ import styled from 'styled-components';
 export const LinkContainer = styled(Link)`
   display: ${({ inline }) => (inline ? 'inline-flex' : 'flex')};
   align-items: center;
-  gap: 5px;
-  margin: 0.5rem 0;
+  gap: 4px;
   white-space: nowrap;
   width: fit-content;
 `;
@@ -13,9 +12,4 @@ export const LinkContainer = styled(Link)`
 export const LinkText = styled.span`
   font-size: 16px;
   font-weight: ${({ theme }) => theme.fonts.weights.bold};
-`;
-export const LinkTextIconContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 4px;
 `;
