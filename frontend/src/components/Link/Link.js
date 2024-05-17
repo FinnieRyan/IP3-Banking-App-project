5c4a6497f38b071a0bfb3dbb52a4ae88fb2ed2f7
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-17 16:43:33 +0100

    UI: navbar fix

--- frontend/src/components/Link/Link.js
+++ frontend/src/components/Link/Link.js
@@ -7,23 +7,23 @@ import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 // STYLES
 import { LinkContainer, LinkText } from './Link.style';
 
-export const Link = ({ children, location, backLink = false, isExternal = false, inline = false }) => {
+export const Link = ({ children, location, backLink = false, isExternal = false, inline = false, white = false }) => {
   return (
     <>
       {isExternal ? (
-        <LinkContainer to={location} target="_blank" rel="noreferrer" inline={inline}>
+        <LinkContainer to={location} target="_blank" rel="noreferrer" inline={inline} white={white}>
           <FiExternalLink style={{ fontSize: '18px' }} />
           <LinkText> {children}</LinkText>
         </LinkContainer>
       ) : (
         <>
           {backLink ? (
-            <LinkContainer to={location} inline={inline} style={{ marginLeft: '-8px' }}>
+            <LinkContainer to={location} inline={inline} style={{ marginLeft: '-8px' }} white={white}>
               {!inline && <FiChevronLeft style={{ fontSize: '24px' }} />}
               <LinkText>{children}</LinkText>
             </LinkContainer>
           ) : (
-            <LinkContainer to={location} inline={inline}>
+            <LinkContainer to={location} inline={inline} white={white}>
               <LinkText>{children}</LinkText>
               {!inline && <FiChevronRight style={{ fontSize: '24px' }} />}
             </LinkContainer>
@@ -40,4 +40,5 @@ Link.propTypes = {
   backLink: PropTypes.bool,
   isExternal: PropTypes.bool,
   inline: PropTypes.bool,
+  white: PropTypes.bool,
 };
