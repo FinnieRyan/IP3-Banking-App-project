7969c84306a154eba63ddc157e4ed01764b3caee
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-17 15:25:03 +0100

    UI: updated page layout

--- frontend/src/components/PageLayout/PageLayout.js
+++ frontend/src/components/PageLayout/PageLayout.js
@@ -1,10 +1,13 @@
 // STYLES
+import { Heading } from '../Heading/Heading';
+import { Link } from '../Link/Link';
 import { Container } from './PageLayout.style';
 
 export const PageLayout = ({ children }) => {
   return (
     <Container>
-      <></>
+      <Link backLink>Back link</Link>
+      <Heading>App heading</Heading>
       {children}
     </Container>
   );
