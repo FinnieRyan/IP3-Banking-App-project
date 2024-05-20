77fcf9e1d3ecd969483b2c3d5da13fd2655dd4c8
FinnieRyan <80767436+FinnieRyan@users.noreply.github.com>
2024-05-20 13:08:08 +0100

    refactor

--- server/server.js
+++ server/server.js
@@ -3,10 +3,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-// Middleware
+
 app.use(bodyParser.json());
 
-// Routes
+
 const protectedRoutes = require('./routes/protected');
 app.use('/api', protectedRoutes);
 
