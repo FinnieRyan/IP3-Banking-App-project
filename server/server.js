1b10174a14e70d99c7e06826bbfdfbb9fddef39d
FinnieRyan <80767436+FinnieRyan@users.noreply.github.com>
2024-05-24 13:23:53 +0100

    refactor

--- server/server.js
+++ server/server.js
@@ -37,6 +37,3 @@ connectToDBAndStartServer()
 mongoose.connection.on('error', (error) => {
   console.error('MongoDB connection error:', error)
 })
-
-//const PORT = process.env.PORT || 5001;
-//app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
