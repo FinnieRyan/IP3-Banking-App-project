f1ba087aa6883fe96ed88a0df30c4000fc4951f8
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-24 12:04:05 +0100

    UI: created action card

---
+++ frontend/src/components/ActionCard/ActionCard.style.js
@@ -0,0 +1,32 @@
+import { AiFillPoundCircle } from 'react-icons/ai';
+import { FiChevronRight } from 'react-icons/fi';
+import styled from 'styled-components';
+import { CardContainer } from '../Card/Card.style';
+
+export const ActionCardContainer = styled(CardContainer)`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+export const IconTextContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+`;
+
+export const StyledIcon = styled(AiFillPoundCircle)`
+  font-size: 38px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
+
+export const StyledChevron = styled(FiChevronRight)`
+  font-size: 38px;
+  margin-right: -12px;
+  color: ${({ theme }) => theme.colors.primary};
+`;
