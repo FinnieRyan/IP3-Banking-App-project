3c711b1b664eb137857144ad7330a77cee5edb64
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-24 13:23:05 +0100

    UI: added links to pages

---
+++ frontend/src/pages/Account/Account.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { PageLayout } from '../../components/PageLayout/PageLayout';
+import { Heading } from '../../components/Heading/Heading';
+
+export const Account = () => {
+  return (
+    <PageLayout>
+      <Heading>Account</Heading>
+    </PageLayout>
+  );
+};
