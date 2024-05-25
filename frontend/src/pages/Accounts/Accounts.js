22c2b4c8f342ed0e0f964a798b5b2305bb187579
Alex Kinsey <alexkinsey@Alexs-MacBook-Pro.local>
2024-05-26 00:11:24 +0100

    UI: created accounts page

--- frontend/src/pages/Accounts/Accounts.js
+++ frontend/src/pages/Accounts/Accounts.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import { PageLayout } from '../../components/PageLayout/PageLayout';
-import { Heading } from '../../components/Heading/Heading';
+import { ActionCard } from '../../components/ActionCard/ActionCard';
+import { Button } from '../../components/Button/Button';
+import pound from '../../assets/pound.svg';
+import { Spacer } from '../../components/ContentLayout/Spacer';
+import { FiPlus } from 'react-icons/fi';
 
 export const Accounts = () => {
   return (
-    <PageLayout>
-      <Heading>Accounts</Heading>
+    <PageLayout linkText="Home" linkLocation={'/'} heading="Accounts">
+      <ActionCard icon={pound} content="Current Account" subContent="£X" />
+      <Spacer />
+      <Button>
+        Transactions <FiPlus style={{ fontSize: '32px', marginRight: '-5px' }} />
+      </Button>
     </PageLayout>
   );
 };
