3634ddcc387c527aa3b854f09a8672f090172f5c
alexkinsey <alexanderkinsey@icloud.com>
2024-06-04 17:28:01 +0100

    UI: created day seperator for account page

--- frontend/src/pages/Account/Account.js
+++ frontend/src/pages/Account/Account.js
@@ -1,45 +1,44 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { PageLayout } from '../../components/PageLayout/PageLayout';
 import { Heading } from '../../components/Heading/Heading';
 import { Card } from '../../components/Card/Card';
 import { useAccounts } from '../../hooks/useAccounts';
 import { useParams } from 'react-router-dom';
-import { FaCircleInfo } from 'react-icons/fa6';
-import { useTheme } from 'styled-components';
 import { HR } from '../../components/HR/HR';
 import { Text } from '../../components/Text/Text';
-import { AccountDetailsContainer } from './Account.style';
+import { AccountDetailsContainer, InfoIcon } from './Account.style';
 import { MonthPicker } from '../../components/MonthPicker/MonthPicker';
 import { ExpensesBars } from '../../components/ExpensesBars/ExpensesBars';
+import { DaySeparator } from '../../components/DaySeparator/DaySeparator';
+import { useTheme } from 'styled-components';
+import { getDatesForMonth } from '../../helpers/getDatesForMonth';
+import { GroupContent } from '../../components/ContentLayout/GroupContent';
 
 export const Account = () => {
   const theme = useTheme();
   const { accountId } = useParams();
   const { getAccountById } = useAccounts();
-  const [selectedMonth, setSelectedMonth] = useState(() => {
-    const date = new Date();
-    return {
-      name: date.toLocaleString('default', { month: 'long' }),
-      year: date.getFullYear(),
-    };
-  });
   const [showAccountDetails, setShowAccountDetails] = useState(false);
   const account = getAccountById(accountId);
+
+  // For the dates and date picker
+  const date = new Date();
+  const [selectedMonth, setSelectedMonth] = useState({
+    name: date.toLocaleString('default', { month: 'long' }),
+    month: date.getMonth(),
+    year: date.getFullYear(),
+  });
+  const [dates, setDates] = useState([]);
+  useEffect(() => {
+    if (selectedMonth.month !== undefined && selectedMonth.year !== undefined) {
+      setDates(getDatesForMonth(selectedMonth.month, selectedMonth.year));
+    }
+  }, [selectedMonth]);
 
   return (
     <PageLayout linkText="Accounts" linkLocation={'/accounts'}>
       <Card style={{ position: 'relative' }}>
-        <FaCircleInfo
-          style={{
-            position: 'absolute',
-            fontSize: '24px',
-            top: '24px',
-            right: '24px',
-            color: theme.colors.grey,
-            cursor: 'pointer',
-          }}
-          onClick={() => setShowAccountDetails(!showAccountDetails)}
-        />
+        <InfoIcon onClick={() => setShowAccountDetails(!showAccountDetails)} />
         <Heading>{account.accountType} Account</Heading>
         {showAccountDetails && (
           <>
@@ -77,6 +76,18 @@ export const Account = () => {
           ]}
         />
       </Card>
+      <GroupContent>
+        {[...dates].reverse().map((date, index) => (
+          <DaySeparator
+            key={index}
+            date={date.toLocaleDateString('en-GB', {
+              // weekday: 'short',
+              day: '2-digit',
+              month: 'short',
+            })}
+          />
+        ))}
+      </GroupContent>
     </PageLayout>
   );
 };
