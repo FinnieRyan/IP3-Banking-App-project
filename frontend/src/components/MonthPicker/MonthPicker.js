e48e91b4dc8fb36597fcfe40a96f36ca35518a79
alexkinsey <alexanderkinsey@icloud.com>
2024-06-04 15:48:30 +0100

    UI: moved styles out

--- frontend/src/components/MonthPicker/MonthPicker.js
+++ frontend/src/components/MonthPicker/MonthPicker.js
@@ -1,66 +1,13 @@
 import React, { useState, useEffect, useRef, useMemo } from 'react';
-import styled from 'styled-components';
 import { debounce } from 'lodash';
 import { generateMonthList } from './generateMonthList';
-
-const MonthPickerContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 100%;
-  margin-bottom: -30px;
-`;
-
-const Months = styled.ul`
-  display: flex;
-  overflow-x: auto;
-  scroll-snap-type: x mandatory;
-  width: 100%;
-  justify-content: flex-start;
-  align-items: center;
-  position: relative;
-
-  /* Hide scrollbar for Chrome, Safari and Opera */
-  ::-webkit-scrollbar {
-    display: none;
-  }
-
-  /* Hide scrollbar for IE, Edge and Firefox */
-  -ms-overflow-style: none; /* IE and Edge */
-  scrollbar-width: none; /* Firefox */
-`;
-
-const Month = styled.li`
-  flex: 0 0 auto;
-  padding: 0 16px;
-  cursor: pointer;
-  font-weight: ${(props) => (props.$active ? 'bold' : '400')};
-  font-size: ${(props) => (props.$active ? '24px' : '18px')};
-  color: ${({ theme, $active }) =>
-    $active ? theme.colors.primary : theme.colors.textBlack};
-  transition: opacity 0.3s;
-  scroll-snap-align: center;
-`;
-
-const Spacer = styled.div`
-  flex: 0 0 auto;
-  width: 50%;
-`;
-
-const Fader = styled.div`
-  position: relative;
-  top: -30px;
-  width: 100%;
-  height: 30px;
-  background: linear-gradient(
-    90deg,
-    rgba(239, 243, 248, 1) 0%,
-    rgba(239, 243, 248, 0) 35%,
-    rgba(239, 243, 248, 0) 75%,
-    rgba(239, 243, 248, 1) 100%
-  );
-  pointer-events: none;
-`;
+import {
+  Fader,
+  Month,
+  MonthPickerContainer,
+  Months,
+  Spacer,
+} from './MonthPicker.style';
 
 export const MonthPicker = ({ startDate, onMonthChange }) => {
   const today = new Date();
